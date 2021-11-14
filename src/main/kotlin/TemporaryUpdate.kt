a5f7a4d5f7405a3845a94a27f74db54f6f5f4a12
maksimmartynov <maxmartynov08@gmail.com>
2021-11-14 21:27:31 +0300

    Add plans for 3rd week November

--- src/main/kotlin/TemporaryUpdate.kt
+++ src/main/kotlin/TemporaryUpdate.kt
@@ -3,16 +3,7 @@ import keyboards.MainKeyboardAfterPayment
 import keyboards.MainKeyboardBeforePayment
 
 suspend fun temporaryUpdate(clientsRepository: ClientsRepository, vkApiClient: VKApiClient) {
-    clientsRepository.update(
-        143964633,
-            newStatus = Status.WAITING_FOR_START,
-            newWeeksPassed = 0,
-            newDaysPassed = 0,
-            newTrainingPlan = TrainingPlan(10, 10, 1),
-            newInterviewResults = mutableListOf(),
-    )
-    vkApiClient.sendMessageSafely(
-        143964633,
-        "Оплата подтверждена! Спасибо, что решили продолжить тренировки по подписке."
-    )
+    clientsRepository.getAll().forEach {
+        println(it)
+    }
 }
